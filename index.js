1b44f676f4c2ec6dfe06e71359d3a0267c04f839
Victor Toma <victor.toma@cgm.com>
2021-06-29 10:01:07 +0300

    resolved BEM pattern

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const hamburgerBtn = document.querySelector(".hamburgerBtn")
+const hamburgerBtn = document.querySelector("#hamburger-btn")
 const menu = document.querySelector(".menu")
 
-const openMenu = () => menu.classList.toggle("menuMobileOpen")
+const openMenu = () => menu.classList.toggle("menu-mobile-open")
 
 hamburgerBtn.addEventListener("click", openMenu)
 
 window.addEventListener("resize", () => {
 	if (window.innerWidth > 700) {
-		return menu.classList.remove("menuMobileOpen")
+		return menu.classList.remove("menu-mobile-open")
 	}
 })
