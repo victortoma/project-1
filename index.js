340d7544a0163d6e684b3360d29496adb8ee3315
Victor Toma <victor.toma@cgm.com>
2021-06-30 17:37:03 +0300

    img modal / accesability h / hr-after

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+const body = document.querySelector("body")
 const hamburgerBtn = document.querySelector("#hamburger-btn")
 const menu = document.querySelector(".menu")
-
+const avatarArr = document.querySelectorAll(".avatar")
+const imgModal = document.querySelector(".img-modal")
 const openMenu = () => menu.classList.toggle("menu-mobile-open")
 
+const displayModal = (event) => {
+	const img = imgModal.firstElementChild
+	console.log(event.target.src)
+	img.src = event.target.src
+	imgModal.addEventListener("click", () => (imgModal.style.display = "none"))
+	return (imgModal.style.display = "block")
+}
+
+avatarArr.forEach((element) => element.addEventListener("click", displayModal))
 hamburgerBtn.addEventListener("click", openMenu)
 
 window.addEventListener("resize", () => {
