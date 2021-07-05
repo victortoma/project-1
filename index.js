45f0cada7c4a29e37d9007e1263697957b900d87
Victor Toma <victor.toma@cgm.com>
2021-07-05 13:44:18 +0300

    flex display/close-modal-span/aspect-ration-modal/remove-scroll/

--- index.js
+++ index.js
@@ -5,23 +5,31 @@ const menu = document.querySelector(".menu")
 const menuBackground = document.querySelector(".menu-mobile-background")
 const avatarArr = document.querySelectorAll(".avatar")
 const imgModal = document.querySelector(".img-modal")
+const closeImgModal = document.querySelector(".close-img-modal")
+const main = document.querySelector(".main")
 
 const displayModal = (event) => {
 	const img = imgModal.firstElementChild
 	img.src = event.target.src
-	imgModal.addEventListener("click", () => (imgModal.style.display = "none"))
-	return (imgModal.style.display = "block")
+	closeImgModal.addEventListener(
+		"click",
+		() => (imgModal.style.display = "none")
+	)
+	return (imgModal.style.display = "flex")
 }
 avatarArr.forEach((element) => element.addEventListener("click", displayModal))
 
 const openMenu = () => {
+	body.style.overflow = "hidden"
 	menuBackground.style.display = "block"
 	menu.classList.toggle("menu-mobile-open")
 }
 const closeMenu = () => {
+	body.style.overflow = "initial"
 	menuBackground.style.display = "none"
 	return menu.classList.remove("menu-mobile-open")
 }
+
 hamburgerBtn.addEventListener("click", openMenu)
 hamburgerBtnClose.addEventListener("click", closeMenu)
 menuBackground.addEventListener("click", closeMenu)
