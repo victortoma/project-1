5d6f93e59b4ae1a26198f5ac4dfd20e62abad701
Victor Toma <victor.toma@cgm.com>
2021-07-08 10:58:16 +0300

    close modal with wrapper event/ close menu with keyboard shortcut

--- index.js
+++ index.js
@@ -4,21 +4,32 @@ const hamburgerBtnClose = document.querySelector(".hamburger-btn-close")
 const menu = document.querySelector(".menu")
 const menuBackground = document.querySelector(".menu-mobile-background")
 const avatarArr = document.querySelectorAll(".avatar")
-const imgModal = document.querySelector(".img-modal")
+const imgModalWrapper = document.querySelector(".img-modal-wrapper")
+const imgModalImg = document.querySelector(".img-modal img")
 const closeImgModal = document.querySelector(".close-img-modal")
 const main = document.querySelector(".main")
 
+document.addEventListener("keydown", function (event) {
+	if (event.ctrlKey && (event.key === "x" || event.key === "X")) {
+		if (!menu.classList.contains("menu-mobile-open")) openMenu()
+		else closeMenu()
+		event.preventDefault() //for what
+	}
+})
+
+imgModalImg.onclick = (e) => e.stopPropagation()
+imgModalWrapper.onclick = () => (imgModalWrapper.style.display = "none")
 const displayModal = (event) => {
-	const img = imgModal.firstElementChild.firstElementChild
+	const img = imgModalWrapper.firstElementChild.firstElementChild
 	img.src = event.target.src
 	closeImgModal.addEventListener(
 		"click",
-		() => (imgModal.style.display = "none")
+		() => (imgModalWrapper.style.display = "none")
 	)
-	return (imgModal.style.display = "grid")
+	return (imgModalWrapper.style.display = "grid")
 }
-avatarArr.forEach((element) => element.addEventListener("click", displayModal))
 
+avatarArr.forEach((element) => element.addEventListener("click", displayModal))
 const openMenu = () => {
 	body.style.overflow = "hidden"
 	menuBackground.style.display = "block"
